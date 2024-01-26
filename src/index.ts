e11932173695d381f9cfed7c05c8b663e845144b
hhao99 <hhao99@gmail.com>
2024-01-26 11:59:05 -0600

    add nodemon

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ const express = require("express");
 const port = 4040;
 const app = express();
 
-// app.use();
+app.use((req, res) => {
+  res.send("Hello");
+});
 app.listen(port, () => {
   console.log("app running on port: " + port);
 });
